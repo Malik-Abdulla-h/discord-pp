56d8bcb30cb9087dcbd7b050f5fdcb2609f7875b
Malik Abdullah <abdmal573@gmail.com>
2024-05-28 14:44:55 +0500

    changed dark color to discord color

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/ui/providers/darktheme";
+import { cn } from "@/lib/utils";
 const font = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
     <html lang="en" suppressHydrationWarning>
-      <body className={font.className}>
+      <body className={cn(font.className, 
+        "bg-white dark:bg-[#313338]"
+      )}>
         <ThemeProvider
         attribute="class"
         defaultTheme="dark"
